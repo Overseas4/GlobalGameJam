297bc4cd32218e6d4de2ef3487217ab5420978d5
rapraprap <goulet.r@hotmail.com>
2019-12-18 23:26:52 -0500

    controller 0.2

--- UnityProject/GlobalGameJam/Assets/Scripts/BobocheController.cs
+++ UnityProject/GlobalGameJam/Assets/Scripts/BobocheController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class BobocheController : MonoBehaviour
 {
-	[SerializeField] float rotationSpeed;
-	[SerializeField] float movingSpeed;
-	void Update()
+	[SerializeField] float rotationSpeed = 1.5f;
+	[SerializeField] float movingSpeed = 0.2f;
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+
+    void Update()
 	{
-		float axis = rotationSpeed * Input.GetAxis("Horizontal");
-		float forward = movingSpeed * Input.GetAxis("Vertical");
+		float axis = rotationSpeed * Input.GetAxis(Horizontal);
+		float forward = movingSpeed * Input.GetAxis(Vertical);
 		transform.Rotate(0f, axis, 0f);
 		transform.position += transform.forward * forward;
 	}
