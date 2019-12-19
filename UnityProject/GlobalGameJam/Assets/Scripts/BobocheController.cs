c8fb0d13099b073d9c74bb6d52a4d992e0f9494f
patrick sauve <pat_de_poulet@hotmail.com>
2019-12-18 22:02:31 -0500

    bobocheController

---
+++ UnityProject/GlobalGameJam/Assets/Scripts/BobocheController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobocheController : MonoBehaviour
+{
+	[SerializeField] float rotationSpeed;
+	[SerializeField] float movingSpeed;
+	void Update()
+	{
+		float axis = rotationSpeed * Input.GetAxis("Horizontal");
+		float forward = movingSpeed * Input.GetAxis("Vertical");
+		transform.Rotate(0f, axis, 0f);
+		transform.position += transform.forward * forward;
+	}
+}
