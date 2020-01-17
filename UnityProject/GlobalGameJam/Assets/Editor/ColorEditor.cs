68e1cc885847e877ef7768ed2614c0e9e580181a
rapraprap <goulet.r@hotmail.com>
2020-01-16 19:15:47 -0500

    Colorable tool

---
+++ UnityProject/GlobalGameJam/Assets/Editor/ColorEditor.cs
@@ -0,0 +1,20 @@
+﻿//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
+
+//[CustomEditor(typeof(Colorable))]
+//public class ColorEditor : Editor 
+//{
+//    [SerializeField]
+//    private List<Color> _colors = new List<Color>();
+
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        Colorable colorable = target as Colorable;
+//        if (GUILayout.Button("Add Color"))
+//        {
+
+//        }
+//    }
+//}
