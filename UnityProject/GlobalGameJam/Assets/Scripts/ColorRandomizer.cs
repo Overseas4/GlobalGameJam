2e7fa51a2ee259a022d5b0765aba84cfa72974a8
rapraprap <goulet.r@hotmail.com>
2020-01-19 21:41:26 -0500

    Ajouter le player Spawner
    Celui-ci permet de mttre des transform sous le spawnwer pour décidé au hazard de ca positio nde départ

---
+++ UnityProject/GlobalGameJam/Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour
+{
+    [SerializeField] private List<Color> colors = new List<Color>(3);
+    private Color RandomColor { get => colors[Random.Range(0, colors.Count)]; }
+
+    void Start()
+    {
+        SetRandomColor();
+    }
+
+    void Update()
+    {
+        ResetOnEscape();
+    }
+
+    void SetRandomColor()
+    {
+        GetComponent<MeshRenderer>().material.color = RandomColor;
+    }
+
+    void ResetOnEscape()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SetRandomColor();
+        }
+    }
+}
---
+++ UnityProject/GlobalGameJam/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    private Transform[] _spawnPoints = null;
+    private GameObject _player = null;
+    private Transform RandomSpawnPoint { get => _spawnPoints[Random.Range(0, _spawnPoints.Length)]; }
+    public GameObject Player { get => _player != null ? _player : _player = Instantiate(Resources.Load("Prefabs/Player") as GameObject); }
+
+    void Awake()
+    {
+        _spawnPoints = GetComponentsInChildren<Transform>();
+    }
+
+    void Start()
+    {
+        SetRandomPosition();
+    }
+
+    void Update()
+    {
+        ResetOnEscape();
+    }
+
+    void SetRandomPosition()
+    {
+        Transform randomSpawnPoint = RandomSpawnPoint;
+        Player.transform.position = randomSpawnPoint.position;
+        Player.transform.rotation = randomSpawnPoint.rotation;
+    }
+
+    void ResetOnEscape()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SetRandomPosition();
+        }
+    }
+}
