383d338bf7842dbe3bd66560c5cd4a9e0c5abc5b
patrick sauve <pat_de_poulet@hotmail.com>
2020-02-02 17:23:18 -0500

    fasf

---
+++ UnityProject/GlobalGameJam/Assets/Scripts/controller/DieCondition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieCondition : MonoBehaviour
+{
+	bool youDie = false;
+
+	[SerializeField] List<Destructible> destructibles = new List<Destructible>();
+
+	[SerializeField] GameObject loseimage;
+
+	void Update()
+	{
+		youDie = true;
+		foreach (var item in destructibles)
+		{
+			if(item.Health >= 0)
+			{
+				youDie = false;
+			}
+		}
+		if (youDie)
+		{
+			loseimage.SetActive(true);
+		}
+	}
+}
