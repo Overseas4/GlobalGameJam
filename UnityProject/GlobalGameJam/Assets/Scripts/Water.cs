4d0addff4f67388d9223c210e0eb260a11718ca9
rapraprap <goulet.r@hotmail.com>
2020-02-29 20:31:24 -0500

    modifying away some issues

--- UnityProject/GlobalGameJam/Assets/Scripts/Water.cs
+++ UnityProject/GlobalGameJam/Assets/Scripts/Water.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-
     [SerializeField] private AnimationCurve _curveY = AnimationCurve.Linear(0, 0, 10, 10);
-    [SerializeField] private float _wavesSpeed = 0.05f;
-    [SerializeField] private float _cycleDuration = 36f;
+    [SerializeField] private float _wavesTextureSpeed = 0.05f;
     [SerializeField] private float _normalWaveScaling = 1f;
-
     [SerializeField] private float _bigWaveScaling = 3.5f;
+
+    [SerializeField] private int _nbWavesBeforeBigWave = 5;
+    [SerializeField] private Vector2 _minMaxDamage = new Vector2(10f, 15f);
+    [SerializeField] private float _damageMultiplierPerBigWave = 1.5f;
 
     private float _scrollNormalY = 0.62f;
     private float _scrollNormalX = 0.35f;
@@ -21,16 +22,13 @@ public class Water : MonoBehaviour
     private float _bigWavesTimer = 0f;
 
     private int _wavesCount = 0;
-    [SerializeField] private int _nbWavesBeforeBigWave = 5;
-    [SerializeField] public Vector2 _minMaxDamage = new Vector2(10f, 15f);
-    [SerializeField] private float _damageMultiplierPerBigWave = 1.5f;
 
     private Renderer _renderer = null;
     private Renderer Renderer { get => _renderer != null ? _renderer : _renderer = GetComponent<Renderer>(); }
     private Vector2 _offset = new Vector2();
     private Vector2 _offset2 = new Vector2();
     private Vector3 _initialPosition;
-    bool isBigWave = false;
+    private bool _isBigWave = false;
 
     void Start()
     {
@@ -41,20 +39,20 @@ public class Water : MonoBehaviour
     void Update()
     {
         _timer += Time.deltaTime;
-        _offset = new Vector2(_timer * _wavesSpeed * _scrollNormalX, _timer * _wavesSpeed * _scrollNormalY);
-        _offset2 = new Vector2(_timer * _wavesSpeed* _scrollNormal2X, _timer * _wavesSpeed * _scrollNormal2Y);
+        _offset = new Vector2(_timer * _wavesTextureSpeed * _scrollNormalX, _timer * _wavesTextureSpeed * _scrollNormalY);
+        _offset2 = new Vector2(_timer * _wavesTextureSpeed* _scrollNormal2X, _timer * _wavesTextureSpeed * _scrollNormal2Y);
         Renderer.material.mainTextureOffset = _offset;
         Renderer.material.SetTextureOffset("_DetailAlbedoMap", _offset2);
         float y = _curveY.Evaluate(Time.timeSinceLevelLoad) * _normalWaveScaling;
         transform.position = Vector3.up * y + _initialPosition;
-        isBigWave = false;
+        _isBigWave = false;
         if (_timer > _nbWavesBeforeBigWave * 10f)
         {
-            if (!isBigWave)
+            if (!_isBigWave)
             {
                 eventController.Instance.OBJ_bigWave.Post(gameObject);
             }
-            isBigWave = true;
+            _isBigWave = true;
             _bigWavesTimer += Time.deltaTime;
             y = _curveY.Evaluate(Time.timeSinceLevelLoad) * _bigWaveScaling;
             transform.position = Vector3.up * y + _initialPosition;
